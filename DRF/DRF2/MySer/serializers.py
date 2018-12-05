634d78b871adc63338c6d0a571cd51d97360dfd7
jich.ji@esoon.com <jich.ji@esoon.com>
2018-12-05 23:34:22 +0800

    上传

--- DRF/DRF2/MySer/serializers.py
+++ DRF/DRF2/MySer/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(label='姓名', max_length=20)
-    age = serializers.IntegerField(label='年龄')
-    score = serializers.IntegerField(label='分数')
+from MySer.models import *
+# class StudentSerializer(serializers.Serializer):
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    # name = serializers.CharField(label='姓名', max_length=20)
+    # age = serializers.IntegerField(label='年龄')
+    # score = serializers.IntegerField(label='分数')
+    class Meta:
+        model = Studnet
+        # fields = ('name', 'age', 'score')
+        fields = '__all__'
