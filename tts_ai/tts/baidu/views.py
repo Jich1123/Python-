1f49425282603abf9e6dad7cf014010b09a703d4
jich.ji@esoon.com <jich.ji@esoon.com>
2018-12-11 23:38:36 +0800

    上传

--- tts_ai/tts/baidu/views.py
+++ tts_ai/tts/baidu/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import StreamingHttpResponse
 from baidu.wordToVoice import *
+import mimetypes
 from wsgiref.util import FileWrapper
+import re
 import os
 import wave
 # Create your views here.
@@ -22,7 +24,7 @@ def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
                 remaining -= len(data)
             yield data
 
-def play(request):
+def play_bak(request):
     # f = wave.open(r'F:\python学习\Python-\tts_ai\tts\baidu\wavs\result.wav', 'rb')
     # params = f.getparams()
     # nframes = params[4]
@@ -36,6 +38,35 @@ def play(request):
     content_type = 'application/octet-stream'
     resp = StreamingHttpResponse(FileWrapper(open(fname, 'rb')), content_type=content_type)
     resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
+
+
+
+def play(request):
+    path = r"F:\python学习\Python-\tts_ai\tts\baidu\wavs\result.wav"
+    """将视频文件以流媒体的方式响应"""
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+    range_match = range_re.match(range_header)
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = first_byte + 1024 * 1024 * 8  # 8M 每片,响应体最大体积
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(file_iterator(path, offset=first_byte, length=length), status=206,
+                                     content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
+    else:
+        # 不是以视频流方式的获取时，以生成器方式返回整个文件，节省内存
+        resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
     resp['Accept-Ranges'] = 'bytes'
     return resp
 
